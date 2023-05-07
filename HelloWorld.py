c2a9558f5b7689cca44d0db036058079af1abdc3
EmWall <72841687+EmWall@users.noreply.github.com>
2023-05-07 16:59:04 -0400

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+chocHi = "Halito!"
+engHi = "Hello"
+spanHi = "Hola"
+greeting = engHi + spanHi + chocHi
+print(greeting) //There will be no spaces or punctuation
+greeting = engHi + "! " + spanHi + "! " + chocHi + "!"
+print(greeting)
